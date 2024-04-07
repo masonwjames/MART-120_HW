125c4e39bc72b81b4d338212753e0041c1b1961d
masonwjames <rusekmason@gmail.com>
2024-04-06 18:44:59 -0600

    Updates

---
+++ Week 9/sketch.js
@@ -0,0 +1,14 @@
+function setup() 
+{
+    createCanvas(500, 600);
+}
+
+function draw() 
+{
+    background(120, 45, 78);
+    textSize(22)
+    text("SELF PORTRAIT", 10, 80);
+
+    //head
+    
+}
