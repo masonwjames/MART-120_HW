618af39b06668aeed35843d86673937794e0a6a6
masonwjames <rusekmason@gmail.com>
2024-04-06 18:56:58 -0600

    Update sketch.js

--- Week 9/sketch.js
+++ Week 9/sketch.js
@@ -7,8 +7,16 @@ function draw()
 {
     background(224, 224, 224);
     textSize(22)
-    text("SELF PORTRAIT", 112, 50);
+    text("SELF PORTRAIT", 112, 30);
 
     //head
+    fill(240, 200, 150);
+    let headWidth = 200;
+    let headHeight = 250;
+    let cornerRadius = 20;
+    rectMode(CENTER);
+    rect(width / 2, height / 2, headWidth, headHeight, cornerRadius);
+    
+
 
 }
